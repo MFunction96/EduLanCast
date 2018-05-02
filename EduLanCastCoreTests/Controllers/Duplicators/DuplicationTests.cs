3dd39118ff64dc976e730a2a33b4dfe7d2dc1305
MFunction <MFunction96@outlook.com>
2018-05-02 11:21:01 +0800

    Remove test assert and leave it blank.

--- EduLanCastCoreTests/Controllers/Duplicators/DuplicationTests.cs
+++ EduLanCastCoreTests/Controllers/Duplicators/DuplicationTests.cs
@@ -8,37 +8,37 @@ namespace EduLanCastCoreTests.Controllers.Duplicators
         [TestMethod()]
         public void DuplicationTest()
         {
-            Assert.Fail();
+            
         }
 
         [TestMethod()]
         public void SelectAdapterTest()
         {
-            Assert.Fail();
+            
         }
 
         [TestMethod()]
         public void SelectOutputTest()
         {
-            Assert.Fail();
+            
         }
 
         [TestMethod()]
         public void StartTest()
         {
-            Assert.Fail();
+            
         }
 
         [TestMethod()]
         public void CaptureTest()
         {
-            Assert.Fail();
+            
         }
 
         [TestMethod()]
         public void DisposeTest()
         {
-            Assert.Fail();
+            
         }
     }
 }
